7a5f84e90f8d70f7b4efa5f150fd1710508a91c6
Koichi Akabe <vbkaisetsu@gmail.com>
2021-08-22 22:04:02 +0900

    Fix docs

--- vaporetto/src/lib.rs
+++ vaporetto/src/lib.rs
@@ -37,7 +37,7 @@
 //!
 //! let mut f = BufReader::new(File::open("model.bin").unwrap());
 //! let model = Model::read(&mut f).unwrap();
-//! let mut predictor = Predictor::new(model, true).dict_overwrap_size(3);
+//! let mut predictor = Predictor::new(model).dict_overwrap_size(3);
 //!
 //! for line in stdin().lock().lines() {
 //!     let s = Sentence::from_raw(line.unwrap()).unwrap();
