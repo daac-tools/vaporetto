3509c8a4b2403b94437d27bdec7958bf05cb5129
Koichi Akabe <vbkaisetsu@gmail.com>
2021-11-19 19:36:58 +0900

    Use Option for dict_pma

--- vaporetto/src/predictor.rs
+++ vaporetto/src/predictor.rs
@@ -20,7 +20,7 @@ use daachorse::DoubleArrayAhoCorasick;
 /// Predictor.
 pub struct Predictor {
     word_pma: DoubleArrayAhoCorasick,
-    dict_pma: DoubleArrayAhoCorasick,
+    dict_pma: Option<DoubleArrayAhoCorasick>,
     word_weights: Vec<Vec<ScoreValue>>,
     dict_weights: Vec<DictWeight>,
     dict_word_wise: bool,
@@ -79,7 +79,11 @@ impl Predictor {
 
         let word_pma = DoubleArrayAhoCorasick::new(words).unwrap();
         let type_pma = DoubleArrayAhoCorasick::new(model.types).unwrap();
-        let dict_pma = DoubleArrayAhoCorasick::new(dict).unwrap();
+        let dict_pma = if dict.is_empty() {
+            None
+        } else {
+            Some(DoubleArrayAhoCorasick::new(dict).unwrap())
+        };
 
         let type_scorer = TypeScorer::new(type_pma, type_weights, model.type_window_size);
 
@@ -213,40 +217,42 @@ impl Predictor {
     }
 
     fn add_dict_scores(&self, sentence: &Sentence, start: usize, ys: &mut [ScoreValue]) {
-        let char_start = if start >= self.dict_window_size {
-            start + 1 - self.dict_window_size
-        } else {
-            0
-        };
-        let text_start = sentence.char_to_str_pos[char_start];
-        let char_end = std::cmp::min(
-            start + ys.len() + self.dict_window_size,
-            sentence.char_to_str_pos.len() - 1,
-        );
-        let text_end = sentence.char_to_str_pos[char_end];
-        let text = &sentence.text[text_start..text_end];
-        let padding = start - char_start + 1;
-        for m in self.dict_pma.find_overlapping_iter(&text) {
-            let m_start = sentence.str_to_char_pos[m.start() + text_start] - char_start;
-            let m_end = sentence.str_to_char_pos[m.end() + text_start] - char_start;
-            let idx = if self.dict_word_wise {
-                m.pattern()
+        if let Some(dict_pma) = self.dict_pma.as_ref() {
+            let char_start = if start >= self.dict_window_size {
+                start + 1 - self.dict_window_size
             } else {
-                std::cmp::min(m_end - m_start, self.dict_weights.len()) - 1
+                0
             };
-            let dict_weight = self.dict_weights[idx];
-            if m_start >= padding && m_start < padding + ys.len() {
-                ys[m_start - padding] += dict_weight.right;
-            }
-            let range_start = std::cmp::max(0, m_start as isize - padding as isize + 1);
-            let range_end = std::cmp::min(m_end as isize - padding as isize, ys.len() as isize);
-            if range_start < range_end {
-                for y in &mut ys[range_start as usize..range_end as usize] {
-                    *y += dict_weight.inner;
+            let text_start = sentence.char_to_str_pos[char_start];
+            let char_end = std::cmp::min(
+                start + ys.len() + self.dict_window_size,
+                sentence.char_to_str_pos.len() - 1,
+            );
+            let text_end = sentence.char_to_str_pos[char_end];
+            let text = &sentence.text[text_start..text_end];
+            let padding = start - char_start + 1;
+            for m in dict_pma.find_overlapping_iter(&text) {
+                let m_start = sentence.str_to_char_pos[m.start() + text_start] - char_start;
+                let m_end = sentence.str_to_char_pos[m.end() + text_start] - char_start;
+                let idx = if self.dict_word_wise {
+                    m.pattern()
+                } else {
+                    std::cmp::min(m_end - m_start, self.dict_weights.len()) - 1
+                };
+                let dict_weight = self.dict_weights[idx];
+                if m_start >= padding && m_start < padding + ys.len() {
+                    ys[m_start - padding] += dict_weight.right;
                 }
-            }
-            if m_end >= padding && m_end < ys.len() + padding {
-                ys[m_end - padding] += dict_weight.left;
+                let range_start = std::cmp::max(0, m_start as isize - padding as isize + 1);
+                let range_end = std::cmp::min(m_end as isize - padding as isize, ys.len() as isize);
+                if range_start < range_end {
+                    for y in &mut ys[range_start as usize..range_end as usize] {
+                        *y += dict_weight.inner;
+                    }
+                }
+                if m_end >= padding && m_end < ys.len() + padding {
+                    ys[m_end - padding] += dict_weight.left;
+                }
             }
         }
     }
