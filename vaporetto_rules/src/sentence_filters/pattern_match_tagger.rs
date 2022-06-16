5268fdf7ca9ffc98da26e2820c16962a0599a41b
Koichi Akabe <vbkaisetsu@gmail.com>
2022-06-16 15:29:36 +0900

    Add tests

--- vaporetto_rules/src/sentence_filters/pattern_match_tagger.rs
+++ vaporetto_rules/src/sentence_filters/pattern_match_tagger.rs
@@ -7,6 +7,7 @@ use vaporetto::Sentence;
 
 use crate::SentenceFilter;
 
+/// Rule based tagger using pattern matching.
 pub struct PatternMatchTagger {
     rules: HashMap<String, Vec<Option<String>>>,
 }
@@ -36,5 +37,27 @@ impl SentenceFilter for PatternMatchTagger {
         for (i, j, tag) in tag_queue {
             sentence.tags_mut()[i * n_tags + j] = tag;
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use alloc::string::String;
+
+    #[test]
+    fn test_concat_grapheme_clusters_no_boundary() {
+        let mut s = Sentence::from_tokenized("これ/名詞/ソレ は テスト/名詞 です//デス").unwrap();
+        let mut rules = HashMap::new();
+        rules.insert("これ".into(), vec![Some("代名詞".into()), Some("コレ".into())]);
+        rules.insert("は".into(), vec![Some("助詞".into()), Some("ワ".into())]);
+        rules.insert("テスト".into(), vec![Some("名詞".into()), Some("テスト".into())]);
+        rules.insert("です".into(), vec![Some("助動詞".into()), Some("デス".into())]);
+        let filter = PatternMatchTagger::new(rules);
+        filter.filter(&mut s);
+        let mut buf = String::new();
+        s.write_tokenized_text(&mut buf);
+        assert_eq!("これ/名詞/ソレ は/助詞/ワ テスト/名詞/テスト です/助動詞/デス", buf);
     }
 }
